bbc661194670628590cba70cca1c03bd30490269
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-04-20 01:31:19 +0200

    DbContext And Configurations

--- E-Commerce API/Program.cs
+++ E-Commerce API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce_API
 {
     public class Program
@@ -13,6 +16,12 @@ namespace E_Commerce_API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+            );
 
             var app = builder.Build();
 
---
+++ Infrastructure/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(P => P.ProductBrand)
+                .WithMany()
+                .HasForeignKey(P => P.BrandId);
+
+
+            builder.HasOne(P => P.ProductType)
+               .WithMany()
+               .HasForeignKey(P => P.TypeId);
+
+            builder.Property(P => P.Price).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
