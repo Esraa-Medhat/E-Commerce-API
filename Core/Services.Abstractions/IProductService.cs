ef8b67802087ab48eac65b305540cd2877916d7c
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-04-23 22:26:11 +0200

    Product Service & Service Manager

---
+++ Core/Services.Abstractions/IProductService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Services.Abstractions
+{
+    public interface IProductService
+    {
+        //Get All Product
+       Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+
+        //Get Product By Id
+       Task<ProductDto?> GetProductByIdAsync(int id);
+
+        //Get All Brand
+        Task<IEnumerable<BrandDto>> GetAllBrandsAsync();
+
+        //Get All Types
+        Task<IEnumerable<TypeDto>> GetAllTypesAsync();
+    }
+}
---
+++ Core/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Entities;
+using Services.Abstractions;
+using Shared;
+
+namespace Services
+{
+    public class ProductService(IUnitOfWork unitOfWork,IMapper mapper) : IProductService
+    {
+        
+
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            //Get All Products through ProductRepository
+           var products=await unitOfWork.GetRepository<Product, int >().GetAllAsync();
+
+            //Mapping IEnumerable<Product> to IEnumerable<ProductDto>
+           var result= mapper.Map<IEnumerable<ProductDto>>(products);
+            return result;
+
+        }
+
+        public async Task<ProductDto?> GetProductByIdAsync(int id)
+        {
+           var product=await  unitOfWork.GetRepository<Product, int>().GetAsync(id);
+            if (product is null) return null;
+            var result = mapper.Map<ProductDto>(product);
+            return result;
+            
+            
+        }
+
+        public async Task<IEnumerable<BrandDto>> GetAllBrandsAsync()
+        {
+           var brands=await unitOfWork.GetRepository<ProductBrand, int>().GetAllAsync();
+           var result= mapper.Map<IEnumerable<BrandDto>>(brands);
+            return result;
+        }
+
+       
+
+        public async Task<IEnumerable<TypeDto>> GetAllTypesAsync()
+        {
+          var types= await  unitOfWork.GetRepository<ProductType, int>().GetAllAsync();
+           var result= mapper.Map<IEnumerable<TypeDto>>(types);
+            return result;
+        }
+
+      
+    }
+}
