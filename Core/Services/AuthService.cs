4772fd8146f557003ef1db054cf8a15889eccb99
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-05-15 02:55:17 +0300

    Generate Jwt Token

--- Core/Services/AuthService.cs
+++ Core/Services/AuthService.cs
@@ -8,6 +8,9 @@ using Domain.Exceptions;
 using Microsoft.AspNetCore.Identity;
 using Services.Abstractions;
 using Shared;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Services
 {
@@ -23,7 +26,7 @@ namespace Services
             {
                 DisplayName = user.DisplayName,
                 Email= user.Email,
-                Token ="TOKEN",
+                Token = await GenerateJwtTokenAsync(user),
             };
           
         }
@@ -48,8 +51,34 @@ namespace Services
             {
                 DisplayName = user.DisplayName,
                 Email = user.Email,
-                Token = "TOKEN",
+                Token =await GenerateJwtTokenAsync(user),
             };
+        }
+        private async Task<string> GenerateJwtTokenAsync(AppUser user)
+        {
+            var authClaim = new List<Claim>()
+            {
+               new Claim (ClaimTypes.Name,user.UserName),
+               new Claim (ClaimTypes.Email,user.Email),
+
+            };
+            var roles = await userManager.GetRolesAsync(user);
+            foreach(var role in roles)
+            {
+                authClaim.Add(new Claim(ClaimTypes.Role, role));
+
+            }
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("StrongSecurityKeyForAuthenticationStrongSecurityKeyForAuthentication"));
+            var token = new JwtSecurityToken(
+               issuer: "https://localhost:7108",
+               audience:"MyAudience",
+               claims:authClaim,
+               expires:DateTime.UtcNow.AddDays(5),
+               signingCredentials:new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature)
+                
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
         }
     }
 }
