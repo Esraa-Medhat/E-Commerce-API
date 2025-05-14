448e529b889272d16a53172992eb4f1a1d93d822
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-05-15 01:54:52 +0300

    Security Module - Auth Controller [Login - Register]

---
+++ Infrastructure/presentation/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared;
+
+namespace presentation
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AuthController(IServiceManager serviceManager):ControllerBase
+    {
+        [HttpPost("login")] // api/Auth/login
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await serviceManager.AuthService.LoginAsync(loginDto);
+            return Ok(result);
+
+        }
+        [HttpPost("register")] // api/Auth/login
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var result = await serviceManager.AuthService.RegisterAsync(registerDto);
+            return Ok(result);
+
+        }
+    }
+}
