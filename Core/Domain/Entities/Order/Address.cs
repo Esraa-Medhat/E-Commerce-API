29e75d49f5bea7235829b4b78aa23e28ee653ceb
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-05-15 18:32:35 +0300

    Order Modules - Models

---
+++ Core/Domain/Entities/Order/Address.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Domain.Entities.Order
+{
+    public class Address
+    {
+        public Address()
+        {
+            
+        }
+        public Address(string firstName, string lastName, string street, string country, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Street = street;
+            Country = country;
+            City = city;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
