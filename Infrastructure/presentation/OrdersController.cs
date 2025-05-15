905dc0d7e9a9dd864152f04cac5a75ff0dbf5662
Esraa Medhat <esraamedhat1315603@gmail.com>
2025-05-16 02:05:59 +0300

    Order Modules - Order Controller

---
+++ Infrastructure/presentation/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Services.Abstractions;
+using Shared.OrdersModels;
+
+namespace presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class OrdersController(IServiceManager serviceManager) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(OrderRequestDto request)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.CreateOrederAsync(request, email);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.GetOrdersByUserEmailAsync(email);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(Guid id)
+        {
+            
+            var result = await serviceManager.OrderService.GetOrderByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet("DeliveryMethods")]
+        public async Task<IActionResult> GetAllDeliveryMethods()
+        {
+           
+            var result = await serviceManager.OrderService.GetAllDeliveryMethods();
+            return Ok(result);
+        }
+    }
+    
+
+    }
